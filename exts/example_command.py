1444634efd7ce5b9e34f579c46f426fc114a5758
Ganoodles <swajabbadaja@gmail.com>
2023-05-22 02:55:35 -0700

    Add explanations and disable example_command.py

--- exts/example_command.py
+++ exts/example_command.py
@@ -1,14 +1,22 @@
-from interactions import Extension, OptionType, cooldown, slash_command, SlashContext, slash_option
+"""ExampleCommand Extension for NoodleBot."""
+
 import interactions
 
-class ExampleCommand(Extension):
-    def __init__(self, bot):
-        self.bot = bot
 
-    @slash_command(name="ping")
-    async def measure_ping(self, ctx):
-        "Measures the bot latency to discord"
+class ExampleCommand(interactions.Extension):
+    """This extension provides a simple ping command to measure bot latency."""
+
+    # we dont need an __init__ here because 
+    # interactions.py automatically assigns self.bot to bot
+
+    # creates a slash command
+    @interactions.slash_command(name="ping")
+    async def measure_ping(self, ctx: interactions.SlashContext):
+        """Measure the bot latency to Discord."""
         await ctx.send('Pong! 🏓')
-    
+
+
 def setup(bot):
-    ExampleCommand(bot)
+    """Set up the ExampleCommand extension."""
+    # uncomment this to enable the extension
+    # ExampleCommand(bot)
